a7336c7e8f614e67a7525540c030743e5e0f9614
yuna706 <riyu6027@naver.com>
2022-04-21 15:33:10 +0900

    async 예제 추가

--- callbackTest.js
+++ callbackTest.js
@@ -11,17 +11,55 @@
 
 //   run();
 //   console.log('Done !!!');
+//----------
 
-function run(callback) {
-    setTimeout(() => {
-        const start = Date.now();
-        for (let i = 0; i < 1000000000; i++) { }
-        const end = Date.now();
-        console.log(end - start + 'ms');
-        callback();
-    }, 1000);
+// function run(callback) {
+//     setTimeout(() => {
+//         const start = Date.now();
+//         for (let i = 0; i < 1000000000; i++) { }
+//         const end = Date.now();
+//         console.log(end - start + 'ms');
+//         callback();
+//     }, 1000);
+// }
+
+// run(() => {
+//     console.log('Done !!!');
+// });
+//----------
+
+// function run() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             const start = Date.now();
+//             for (let i = 0; i < 1000000000; i++) {}
+//             const end = Date.now();
+//             console.log(end - start + 'ms');
+//             resolve();
+//         }, 1000);
+//     });
+// }
+ 
+// run().then(() => {console.log('Done !!!');});
+// run().then(() => {return run()}).then(() => {console.log('Done !!!');});
+// -------------
+
+function run() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const start = Date.now();
+            for (let i = 0; i < 1000000000; i++) {}
+            const end = Date.now();
+            console.log(end - start + 'ms');
+            resolve();
+        }, 1000);
+    });
 }
-
-run(() => {
+ 
+async function process() {
+    await run();
+    await run();
     console.log('Done !!!');
-});
+}
+ 
+process();
